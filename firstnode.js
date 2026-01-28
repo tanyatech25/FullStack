8ae6ed0838876b784af7c08f9d2ee7535bbd85ee
Tanya agrawal <tanya.agrawal_cs24@gla.ac.in>
2026-01-28 23:05:22 +0530

    first commit

---
+++ firstnode.js
@@ -0,0 +1,26 @@
+// function infort(){
+//     console.log("tanya");
+// }
+// function info(){
+//     console.log("agrawal");
+// }
+// export{infort,info};
+// //export.add=()=>{}
+// //module.exports= fuonction add(){}
+
+
+
+// function add(){
+//     console.log("tanya")
+// }
+// module.exports= {add:add};
+
+
+
+
+// function add(){
+//     console.log("tanya")
+// }
+// module.exports.add= add;
+
+//loupe browser
---
+++ second.js
@@ -0,0 +1,8 @@
+// const data = require('./firstnode.js');
+// data.infort();
+// data.info();
+
+
+
+const data = require('./firstnode.js');
+data.add();
